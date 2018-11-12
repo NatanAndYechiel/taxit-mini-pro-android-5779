d4e45965450b85fc085014bbcc5477e0c7c95ed9
yechiel chabany <Yechel@users.noreply.github.com>
2018-11-12 18:38:15 +0200

    #Add Driver, Client and ClientRequest classes

---
+++ app/src/main/java/com/taxit/mini_pro_android_5779/model/entities/Driver.java
@@ -0,0 +1,60 @@
+package com.taxit.mini_pro_android_5779.model.entities;
+
+public class Driver {
+    private String name;
+    private String family;
+    private String id;
+    private String phone;
+    private String mail;
+    private String creditCard;
+
+    //------------------------getters/setters---------------------//
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(String creditCard) {
+        this.creditCard = creditCard;
+    }
+
+}
